b5c20fdb09eb56fc86e208043f7c72efb15324d3
EmbeddedMan <brian@schmalzhaus.com>
2019-10-16 20:30:42 -0500

    Updated address scanner PicoC program to work properly with new I2C return value.

--- E3-Thermal_Sensor_v2/PicoCAddressScanner.c
+++ E3-Thermal_Sensor_v2/PicoCAddressScanner.c
@@ -6,16 +6,17 @@ int data;
 int x;
 int sa;
 
-for (x = 1; x <= 127; x++)
+for (x = 0x01; x <= 0x7F; x++)
 {
-  data = readi2crs(x, 0x00);
+  // data = readi2c(x, 0x00);     // The normal version
+  data = readi2crs(x, 0x00);      // The repeated start version
 
-  if (data != -1)
+  if (data >= 0)
   {
     printf("Found a something at address 0x%02x ", x);
-    
+  
     sa = readi2c3rs(x, 0x2E); // Read SA address from Thermal Sensor
-    
+  
     if (sa > 0)
     {
       // Have we found a thermal sensor?
